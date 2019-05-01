0dcc47805bf37bc3b059ed3b8a9c6c86a9df435c
Alan Cavalcanti <alankmc@gmail.com>
2019-04-30 17:01:00 -0700

    Added webpack-driven local npm publishing

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './styles/main.scss';
+import Button from './button/button';
+
+export { Button };
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
-module.exports = {
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist/'),
+    filename: 'bundle.js',
+    libraryTarget: 'commonjs2', // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+  },
   module: {
     rules: [
       {
@@ -8,7 +16,13 @@ module.exports = {
         query: {
           presets: ['@babel/react', '@babel/preset-env'],
         },
-      }
-    ]
-  }
-}
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
+
+module.exports = config;
