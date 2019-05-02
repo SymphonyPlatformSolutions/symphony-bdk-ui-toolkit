a8bc5b26f1e68a5b31a422ea373333f16e949e14
Ricardo Manoel Junior <rdomanoel@gmail.com>
2019-05-02 12:11:56 -0300

    dev: initial styled button, and example page to verify the components on development

---
+++ examples/example-page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '../src/components/button/button';
+
+const ExamplePage = () => (
+  <div>
+    <Button />
+  </div>
+);
+
+export { ExamplePage };
---
+++ src/components/button/styled-button.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  font-size: 0.75rem;
+  font-weight: bold;
+  border-radius: 4px;
+  min-width: 3rem;
+  min-height: 1.4rem;
+  padding: 0.4rem 1rem;
+  cursor: pointer;
+  transition: all 0.2s ease-in;
+`;
+
+export { StyledButton };
