9887c6e8bd11fbd46e6833bb7e348cf27644a2d1
Ricardo Manoel Junior <rdomanoel@gmail.com>
2019-05-02 11:25:54 -0300

    dev: removing some unnecessary lines, add styled-components dependecy

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ const config = {
   output: {
     path: path.resolve(__dirname, './dist/'),
     filename: 'bundle.js',
-    libraryTarget: 'commonjs2', // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
   },
   module: {
     rules: [
@@ -16,10 +15,6 @@ const config = {
         query: {
           presets: ['@babel/react', '@babel/preset-env'],
         },
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
