20bf8dec670358f648e6adf0fda2466a9ff94c5f
Ricardo Manoel Junior <rdomanoel@gmail.com>
2019-06-05 14:44:38 -0300

    dev: add knobs on stories of Checkbox

--- src/components/base/CheckBox/stories.js
+++ src/components/base/CheckBox/stories.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, text } from '@storybook/addon-knobs';
 import Checkbox from '.';
 import Box from '../Box';
 import Text from '../Text';
@@ -21,12 +22,34 @@ const CheckBoxComponent = () => {
   );
 };
 
+const CheckBoxWithKnobs = () => {
+  const [isChecked, setCheckMark] = useState(true);
+
+  function handleCheckMark(e) {
+    setCheckMark(e.target.checked);
+  }
+
+  return (
+    <Checkbox
+      isChecked={isChecked}
+      onChange={handleCheckMark}
+      disabled={false}
+      label={text('Insert Text:')}
+    />
+  );
+};
+
 storiesOf('Base', module)
+  .addDecorator(withKnobs)
   .add('Checkbox', () => (
     <Box p={15}>
       <Box vertical space={20}>
         <Text title size="large">Default CheckBox</Text>
         <CheckBoxComponent />
+      </Box>
+      <Box vertical space={20}>
+        <Text title size="large">Default CheckBox with Knobs Addon</Text>
+        <CheckBoxWithKnobs />
       </Box>
       <Box vertical space={20}>
         <Text title size="large">Disabled CheckBox</Text>
