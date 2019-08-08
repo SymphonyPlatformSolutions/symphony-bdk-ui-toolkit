d0616af730ce403d1336cb6e4d6514246c73c756
Rovaris <lrovaris@daitan.com>
2019-08-08 19:07:08 -0300

    feat(atomic): bomb

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Box } from './base/box';
-export { default as Button } from './base/button';
-export { default as Card } from './base/card';
-export { default as Loader } from './base/loader';
-export { default as Separator } from './base/separator';
-export { default as Tabs } from './base/tabs';
-export { default as Text } from './base/text';
-export { default as InputField } from './base/input-field';
-export { default as Checkbox } from './base/checkbox';
