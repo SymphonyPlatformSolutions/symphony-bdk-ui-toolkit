b84bd18d7c37b3ab18fcbcff2f0e445708b14b05
rovaris <lrovaris@daitan.com>
2019-09-28 23:08:48 -0300

    feat(help-page): organized code

---
+++ src/components/base/help-page-builder/theme.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ArrowRightCircle } from 'styled-icons/feather';
+import Card from '../card';
+
+export const HelperLink = styled.span`
+  width: fit-content;
+  color: ${({ theme }) => theme.colors.link};
+  text-decoration: none;
+  :hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+`;
+
+
+export const StyledArrowRightCircle = styled(ArrowRightCircle)`
+  width: 32px;
+  height: 32px;
+  color: ${({ theme }) => theme.colors.primary}
+  opacity: 0.7;
+  transition: ease opacity .2s;
+  &:hover {
+    opacity: 1;
+  }
+`;
+export const StyledCard = styled(Card)`
+  height: 35px;
+  max-height: 150px;
+`;
+
+export const StyledSubTopicContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const StyledSubTopic = styled(Card)`
+  width: 240px;
+  height: 128px;
+`;
+
+export const HelpPageContainer = styled.div`
+  width: 42rem;
+`;
