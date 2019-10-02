f37cf07fb1bb1318849094c1a4edc179b06e0e81
Alan Cavalcanti <alankmc@gmail.com>
2019-10-01 17:08:44 -0700

    Feature(PhoneInput): Updated PhoneInput to Material-like UI

--- src/components/base/phone-input-field/index.js
+++ src/components/base/phone-input-field/index.js
@@ -6,6 +6,9 @@ import './assets/flags.css';
 
 import {
   PhoneInputWrapper,
+  InputLine,
+  InputLabel,
+  Container,
 } from './theme';
 
 import { NoOp } from '../../../utils/helpers';
@@ -13,40 +16,47 @@ import { NoOp } from '../../../utils/helpers';
 const PhoneInputField = ({
   value, defaultValue, inputState, disabled, onChange,
   disableAreaCodes, id, onBlur, errorMessage, hasSearchField,
+  onFocus, label,
   ...rest
 }) => {
   const [hasRef, setRef] = useState(null);
+  const [isFocused, setFocus] = useState(false);
   const elRef = createRef();
 
   useEffect(() => {
     setRef(elRef.current);
   }, [elRef, elRef.current]);
-
+  const error = inputState === 'error';
   return (
-    <ErrorWrapper error={inputState === 'error'} errorMessage={errorMessage}>
-      <PhoneInputWrapper
-        ref={elRef}
-        hasRef={hasRef}
-        disabled={disabled}
-        inputState={inputState}
-      >
-        <ReactPhoneInput
-          disableAreaCodes={disableAreaCodes}
+    <Container>
+      <ErrorWrapper error={error} errorMessage={errorMessage}>
+        <PhoneInputWrapper
+          ref={elRef}
+          hasRef={hasRef}
           disabled={disabled}
-          defaultCountry={hasSearchField ? null : defaultValue}
-          value={value}
-          onChange={onChange}
-          onBlur={onBlur}
-          enableSearchField={hasSearchField}
-          disableSearchIcon
-          searchPlaceholder="Type country name"
-          inputExtraProps={{
-            id,
-          }}
-          {...rest}
-        />
-      </PhoneInputWrapper>
-    </ErrorWrapper>
+          inputState={inputState}
+        >
+          <ReactPhoneInput
+            disableAreaCodes={disableAreaCodes}
+            disabled={disabled}
+            defaultCountry={hasSearchField ? null : defaultValue}
+            value={value}
+            onChange={onChange}
+            onBlur={(...e) => { setFocus(false); onFocus(...e); }}
+            onFocus={(...e) => { setFocus(true); onFocus(...e); }}
+            enableSearchField={hasSearchField}
+            disableSearchIcon
+            searchPlaceholder="Type country name"
+            inputExtraProps={{
+              id,
+            }}
+            {...rest}
+          />
+          <InputLabel disabled={disabled} error={error}>{label}</InputLabel>
+          <InputLine focused={isFocused} disabled={disabled} error={error} />
+        </PhoneInputWrapper>
+      </ErrorWrapper>
+    </Container>
   );
 };
 
@@ -62,6 +72,8 @@ PhoneInputField.propTypes = {
   disableAreaCodes: PropTypes.bool,
   errorMessage: PropTypes.string,
   hasSearchField: PropTypes.bool,
+  onFocus: PropTypes.func,
+  label: PropTypes.string,
 };
 
 PhoneInputField.defaultProps = {
@@ -76,6 +88,8 @@ PhoneInputField.defaultProps = {
   disableAreaCodes: true,
   errorMessage: 'Something went wrong!',
   hasSearchField: false,
+  onFocus: NoOp,
+  label: 'Phone number',
 };
 
 export default PhoneInputField;
--- src/components/base/phone-input-field/theme.js
+++ src/components/base/phone-input-field/theme.js
@@ -1,4 +1,4 @@
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 import styled from 'styled-components';
 import { THEME_TYPES } from '../../../styles/colors';
 
@@ -28,16 +28,7 @@ const getPlaceholderColor = ({ theme, inputState }) => (
     : theme.colors.darkgrey
 );
 
-const getBackgroundColor = ({ theme }) => (
-  theme.mode === THEME_TYPES.DARK
-    ? theme.colors.inputgrey
-    : 'transparent'
-);
-
-const getInputFocusBorderColor = ({ theme, inputState }) => (inputState === 'error'
-  ? theme.colors.danger
-  : theme.colors.primary
-);
+const getBackgroundColor = ({ theme, disabled }) => (theme.mode === THEME_TYPES.DARK ? (disabled ? transparentize(0.86, darken(0.7, theme.colors.white)) : transparentize(0.86, darken(0.4, theme.colors.white))) : 'transparent');
 
 const overrides = {
   container: 'react-tel-input',
@@ -58,6 +49,10 @@ export const PhoneInputWrapper = styled.div.attrs(overrides)`
   .${overrides.container} {
      width: 100%;
      border-radius: 0;
+     border: none;
+     & > input {
+       font-style: ${({ disabled }) => (disabled ? 'italic !important' : 'normal')};
+     }
   }
 
   .${overrides.formControl} {
@@ -66,7 +61,7 @@ export const PhoneInputWrapper = styled.div.attrs(overrides)`
     height: 40px !important;
     padding-left: 57px !important;
     width: 100% !important;
-    border: 1px solid ${props => getThemedBorderColor(props)} !important;
+    border: none !important;
     border-radius: 0px !important;
     color: ${props => getColor(props)};
     background-color: ${props => getBackgroundColor(props)} !important;
@@ -83,7 +78,14 @@ export const PhoneInputWrapper = styled.div.attrs(overrides)`
 
     &:focus {
      outline: none;
-     border-color: ${props => getInputFocusBorderColor(props)} !important;
+    }
+
+    &:hover {
+      background-color: ${({ theme, disabled }) => (theme.mode === THEME_TYPES.DARK
+    ? disabled
+      ? undefined
+      : `${transparentize(0.82, darken(0.15, theme.colors.white))} !important`
+    : 'transparent !important')};
     }
   }
 
@@ -94,7 +96,7 @@ export const PhoneInputWrapper = styled.div.attrs(overrides)`
     .selected-flag {
       background-color: transparent !important;
     }
-
+  filter: ${({ disabled }) => (disabled ? 'grayscale(100%)' : undefined)};
   }
 
   .${overrides.searchHover} {
@@ -126,6 +128,7 @@ export const PhoneInputWrapper = styled.div.attrs(overrides)`
     background-color: ${props => getDropdownBackgroundColor(props)} !important;
     border: 1px solid ${props => getThemedBorderColor(props)} !important;
     width: ${props => getDropdownWidth(props)} !important;
+    margin: 0 0 10px -1px !important;
     .search {
       position: relative !important;
       padding: 5px 4px !important;
@@ -140,26 +143,80 @@ export const PhoneInputWrapper = styled.div.attrs(overrides)`
       border-radius: 0px !important;
     }
     .dial-code {
-      color: ${({ theme }) => darken(0.5, theme.colors.lightgrey)} !important;
+      color: ${({ theme }) => darken(0.5, theme.colors.textcolor)} !important;
     }
     .country {
-      height: 40px !important;
+      height: 34px !important;
     }
     .country:hover {
-      color: ${({ theme }) => theme.colors.lightgrey} !important;
+      color: ${({ theme }) => theme.colors.textcolor} !important;
       background-color: ${({ theme }) => theme.colors.secondary} !important;
      .dial-code {
-        color: ${({ theme }) => theme.colors.lightgrey} !important;
+        color: ${({ theme }) => theme.colors.textcolor} !important;
       }
     }
 
     .highlight {
-      color: ${({ theme }) => theme.colors.lightgrey} !important;
+      color: ${({ theme }) => theme.colors.textcolor} !important;
       .dial-code {
-        color: ${({ theme }) => theme.colors.lightgrey} !important;
+        color: ${({ theme }) => theme.colors.textcolor} !important;
       }
       background-color: ${({ theme }) => theme.colors.primary} !important;
     }
   }
- }
+ 
+ position: relative;
+`;
+
+function getLineColor(props) {
+  const { theme } = props;
+
+  return theme.colors.textcolor;
+}
+
+export const InputLine = styled.span`
+  width: 100%;
+  display: block;
+  position: absolute;
+  bottom: 0;
+
+  &:before {
+    content: "";
+    height: 1px;
+    width: 100%;
+    bottom: 0;
+    position: absolute;
+    border-bottom: 1px ${({ disabled }) => (disabled ? 'dotted' : 'solid')}
+    ${props => getLineColor(props)};
+  }
+
+  div:hover:not(:disabled) ~ &:before {
+    border-bottom: ${props => (props.disabled ? undefined : `2px solid ${getLineColor(props)}`)};
+  }
+
+  &:after {
+    content: "";
+    height: 2px;
+    width: ${({ error, focused }) => (error || focused ? '100%' : 0)};
+    bottom: 0;
+    position: absolute;
+    background: ${({ error, theme }) => (error ? theme.colors.danger : theme.colors.primary)};
+    transition: all cubic-bezier(0.075, 0.82, 0.165, 1) 0.3s;
+  }
+`;
+
+export const InputLabel = styled.label`
+  position: absolute;
+  left: 7px;
+  font-size: 1rem;
+  transition: all 0.2s;
+  font-style: ${({ disabled }) => (disabled ? 'italic' : 'normal')};
+  color: ${({ theme, error, disabled }) => (disabled ? theme.colors.darkgrey : (error ? theme.colors.danger : theme.colors.primary))};
+  top: -14px;
+  font-size: 12px;
+  z-index: 1;
+`;
+
+export const Container = styled.div`
+  margin-top: 7px;
 `;
