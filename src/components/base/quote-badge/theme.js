626b35d8eceb2278353dd5fc0189eb9e19dcfb68
Leandro Ortiz <lortiz@daitan.com>
2019-10-30 16:04:26 -0300

    feat(sparc): Badges according to Figma

--- src/components/base/quote-badge/theme.js
+++ src/components/base/quote-badge/theme.js
@@ -1,24 +1,44 @@
 import styled from 'styled-components';
 import Box from '../box';
 import Text from '../text';
+import { THEME_TYPES } from '../../../styles/colors';
+
+const getBadgeColor = ({ theme }) => (
+  theme.mode === THEME_TYPES.LIGHT
+    ? '#E0EBFC'
+    : '#B5BACA'
+);
+
+const getBadgeBorderColor = ({ theme }) => (
+  theme.mode === THEME_TYPES.LIGHT
+    ? '#C3D5F0'
+    : '#596484'
+);
+
+const getTextColor = ({ theme }) => (
+  theme.mode === THEME_TYPES.LIGHT
+    ? '#19376B'
+    : '#000028'
+);
 
 export const BaseBadge = styled(Box)`
   border-radius: 2px;
-  padding: 4px;
-  background-color: ${({ theme }) => (theme.colors.lightgrey)};
+  padding: 4px 8px;
+  background-color: ${props => getBadgeColor(props)};
+  border: ${props => `1px solid ${getBadgeBorderColor(props)}`};
   width: fit-content;
 `;
 
 export const SideInfo = styled(Text)`
   font-size: 8px;
   margin-right: 4px;
-  color: #4d4d4d;
+  color: ${props => getTextColor(props)};
 `;
 
 export const MainInfo = styled(Text)`
-  font-size: 12px;
+  font-size: 16px;
   font-weight: bold;
-  color: #4d4d4d;
+  color: ${props => getTextColor(props)};
 `;
 
 export const IconButton = styled.button`
@@ -26,6 +46,6 @@ export const IconButton = styled.button`
   margin: 1px 0 0 4px;
   border: none;
   background: none;
-  color: #4d4d4d;
+  color: ${props => getTextColor(props)};
   cursor: pointer;
 `;
