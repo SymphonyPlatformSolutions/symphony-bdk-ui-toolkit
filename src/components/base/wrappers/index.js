f96695d99f321ca55579cd3cd98bba671fdadfc1
Rovaris <lrovaris@daitan.com>
2019-11-28 17:54:24 -0300

    style(story-wrapper): added fill available for heights

--- src/components/base/wrappers/index.js
+++ src/components/base/wrappers/index.js
@@ -4,6 +4,7 @@ import Box from '../box';
 export const StoryWrapper = styled(Box)`
   padding-top: 20px;
   padding-left: 20px;
+  height: -webkit-fill-available;
   background-color: ${({ theme }) => (
     theme.colors.mainbackground)};
 `;
