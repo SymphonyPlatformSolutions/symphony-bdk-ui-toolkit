5d19f5066735e3dd7fed4fb9c22e550f4931fb65
Lucas Rovaris <lucas.rovaris@devfactory.com>
2019-12-04 15:14:38 -0300

    feat(wip): working

--- src/components/base/icons/stories.js
+++ src/components/base/icons/stories.js
@@ -3,6 +3,7 @@ import { storiesOf } from '@storybook/react';
 import Box from '../box';
 import Text from '../text';
 import { StoryWrapper } from '../wrappers';
+import styled from 'styled-components';
 import {
   SecurityIcon,
   SettingsIcon,
@@ -25,6 +26,25 @@ import {
   CloseIcon,
 } from '.';
 
+const TestWrapper = styled.div`
+  background-color: red;
+`
+
+const TestContainer = styled.div`
+  background: linear-gradient(to top left,
+             rgba(0,0,0,0) 0%,
+             rgba(0,0,0,0) calc(50% - 0.8px),
+             rgba(0,0,0,1) 50%,
+             rgba(0,0,0,0) calc(50% + 0.8px),
+             rgba(0,0,0,0) 100%),
+         linear-gradient(to top right,
+             rgba(0,0,0,0) 0%,
+             rgba(0,0,0,0) calc(50% - 0.8px),
+             rgba(0,0,0,1) 50%,
+             rgba(0,0,0,0) calc(50% + 0.8px),
+             rgba(0,0,0,0) 100%);
+`
+
 storiesOf('Base', module).add(
   'Icons',
   () => React.createElement(() => (
@@ -33,85 +53,161 @@ storiesOf('Base', module).add(
         <Text isTitle>Icons</Text>
         <Box horizontal>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <SettingsIcon />
+          </TestContainer>
+          </TestWrapper>
             <Text>Settings</Text>
           </Box>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <SecurityIcon />
+          </TestContainer>
+          </TestWrapper>
             <Text>Security</Text>
           </Box>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <NotificationsIcon />
+          </TestContainer>
+          </TestWrapper>
             <Text>Notifications</Text>
           </Box>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <InteractionsIcon />
+          </TestContainer>
+          </TestWrapper>
             <Text>Interactions</Text>
           </Box>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <CloudIcon />
-            <Text>Cloud</Text>
+
+          </TestContainer>
+          </TestWrapper><Text>Cloud</Text>
           </Box>
         </Box>
         <Box horizontal>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <ServerIcon />
+          </TestContainer>
+          </TestWrapper>
             <Text>Server</Text>
           </Box>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <EyeIcon />
-            <Text>Eye</Text>
+
+          </TestContainer>
+          </TestWrapper><Text>Eye</Text>
           </Box>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <ClosedEyeIcon />
+          </TestContainer>
+          </TestWrapper>
             <Text>Closed Eye</Text>
           </Box>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <EditIcon />
-            <Text>Edit</Text>
+
+          </TestContainer>
+          </TestWrapper><Text>Edit</Text>
           </Box>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <DeleteIcon />
+          </TestContainer>
+          </TestWrapper>
             <Text>Delete</Text>
           </Box>
         </Box>
         <Box horizontal>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <DownChevron />
+          </TestContainer>
+          </TestWrapper>
             <Text>Down Chevron</Text>
           </Box>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <TickIcon />
-            <Text>Tick</Text>
+
+          </TestContainer>
+          </TestWrapper><Text>Tick</Text>
           </Box>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <CrossIcon />
-            <Text>Cross</Text>
+
+          </TestContainer>
+          </TestWrapper><Text>Cross</Text>
           </Box>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <PricingIcon />
+          </TestContainer>
+          </TestWrapper>
             <Text>Pricing</Text>
           </Box>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <InfoIcon />
-            <Text>Info</Text>
+
+          </TestContainer>
+          </TestWrapper><Text>Info</Text>
           </Box>
         </Box>
         <Box horizontal>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <EmptyStarIcon />
+          </TestContainer>
+          </TestWrapper>
             <Text>Empty Star</Text>
           </Box>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <StarIcon />
-            <Text>Star</Text>
+
+          </TestContainer>
+          </TestWrapper><Text>Star</Text>
           </Box>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <CloseIcon />
-            <Text>Close</Text>
+
+          </TestContainer>
+          </TestWrapper><Text>Close</Text>
           </Box>
           <Box type="flat" justify="center" align="center">
+          <TestWrapper>
+            <TestContainer>
             <EllipsisIcon />
+          </TestContainer>
+          </TestWrapper>
             <Text>Ellipsis</Text>
           </Box>
         </Box>
