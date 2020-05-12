563ebc18cc14ec1a5f27d7f2167e9c6fcf944f1a
Alan Cavalcanti <alankmc@gmail.com>
2020-05-11 17:16:22 -0700

    Feature(Datepicker): Added datepicker Portal library

--- src/components/inputs/datepicker/theme.js
+++ src/components/inputs/datepicker/theme.js
@@ -4,99 +4,77 @@ import InputField from '../input-field';
 import Text from '../../misc/text';
 
 export const MonthTitleContainer = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-margin-top: 8px;
-margin-bottom: 12px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 8px;
+  margin-bottom: 12px;
 `;
 export const ChangeMonthButton = styled.button`
-margin: 0 12px;
-background-color: transparent;
-border: none;
-cursor: pointer;
-visibility: ${({ show }) => (show ? 'visible' : 'hidden')};
-transform: ${({ turnLeft }) => (turnLeft ? 'rotate(90deg)' : 'rotate(-90deg)')};
+  margin: 0 12px;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  visibility: ${({ show }) => (show ? 'visible' : 'hidden')};
+  transform: ${({ turnLeft }) => (turnLeft ? 'rotate(90deg)' : 'rotate(-90deg)')};
 `;
 export const WeekSeparator = styled.div`
-display: grid;
-grid-template-columns: repeat(7, 1fr);
-justify-content: center;
-margin-top: ${({ margin }) => `${margin}px`};
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  justify-content: center;
+  margin-top: ${({ margin }) => `${margin}px`};
 `;
 export const TitleText = styled(Text)`
-color: ${({ theme }) => theme.colors.grey_600};
-font-weight: bold;
-white-space: nowrap;
+  color: ${({ theme }) => theme.colors.grey_600};
+  font-weight: bold;
+  white-space: nowrap;
 `;
 export const WeekdayTextWrapper = styled.div`
-display: flex;
-justify-content: center;
+  display: flex;
+  justify-content: center;
 `;
 export const WeekdayText = styled(Text)`
-font-weight: bold;
-padding-top: 2px;
-transition: all 0.3s;
+  font-weight: bold;
+  padding-top: 2px;
+  transition: all 0.3s;
 `;
 export const WeekdayBubble = styled.div`
-height: 26px;
-width: 26px;
-display: flex;
-border-radius: 50%;
-background-color: ${({ hilighted, theme }) => (hilighted ? transparentize(0.6, theme.colors.primary_400) : 'transparent')};
-align-items: center;
-transition: all 0.3s;
-justify-content: center;
+  height: 26px;
+  width: 26px;
+  display: flex;
+  border-radius: 50%;
+  background-color: ${({ hilighted, theme }) => (hilighted ? transparentize(0.6, theme.colors.primary_400) : 'transparent')};
+  align-items: center;
+  transition: all 0.3s;
+  justify-content: center;
 `;
-export const fadeDown = keyframes`
-  0% {
-    opacity: 0;
-    transform: translateY(90%);
-  }
-  100% {
-    opacity: 1;
-    transform: translateY(100%);
-  }
-`;
-export const fadeDownOut = keyframes`
- 100% {
-    opacity: 0;
-    transform: translateY(90%);
-  }
-  0% {
-    opacity: 1;
-    transform: translateY(100%);
-  }
-`;
-
-export const fadeisUp = keyframes`
-  0% {
-    opacity: 0;
-    transform: translateY(-90%);
-  }
-  100% {
-    opacity: 1;
-    transform: translateY(-100%);
-  }
-`;
-
-export const fadeisUpOut = keyframes`
- 100% {
-    opacity: 0;
-    transform: translateY(-90%);
-  }
-  0% {
-    opacity: 1;
-    transform: translateY(-100%);
-  }
-`;
-
-export const getAnimation = ({ out, isUp }) => {
-  if (isUp) {
-    return out ? fadeisUpOut : fadeisUp;
-  }
-  return out ? fadeDownOut : fadeDown;
+const getUpTransform = ({ heightDelta }) => {
+  const base = -18 + heightDelta;
+  return `${base}px`;
 };
+const fadeIn = ({ isUp, relatedShift }) => keyframes`
+  0% {
+    opacity: 0;
+    transform: translateX(calc(-50% + ${relatedShift}px)) translateY(0);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(calc(-50% + ${relatedShift}px))translateY(${isUp ? '-18px' : '18px'});
+  }
+`;
+const fadeOut = ({ isUp, relatedShift, heightDelta }) => keyframes`
+100% {
+  opacity: 0;
+  transform: translateX(calc(-50% + ${relatedShift}px)) translateY(0);
+}
+0% {
+  opacity: 1;
+  transform: translateX(calc(-50% + ${relatedShift}px))translateY(${isUp ? getUpTransform({ heightDelta }) : '18px'});
+}
+`;
+const getAnimation = ({
+  out, isUp, relatedShift, heightDelta,
+}) => (out ? fadeOut({ isUp, relatedShift, heightDelta }) : fadeIn({ isUp, relatedShift }));
 
 export const CalendarBubble = styled.div`
   background-color: ${({ theme }) => theme.colors.mainbackground};
@@ -105,18 +83,13 @@ export const CalendarBubble = styled.div`
   border: 1px solid ${({ theme }) => theme.colors.grey_200};
   box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.1);
   display: flex;
-  position: absolute;
-  z-index: 10;
-  margin-top: ${({ isUp }) => (isUp ? 0 : '10px')};
-  margin-bottom: ${({ isUp }) => (isUp ? '10px' : 0)};
-  transform: ${({ isUp }) => (isUp ? 'translateY(-100%)' : 'translateY(100%)')};
-  bottom: ${({ isUp }) => (isUp ? undefined : '-14px')};
-  top: ${({ isUp }) => (isUp ? '-14px' : undefined)};
+  transform: translateX(calc(-50% + ${({ relatedShift }) => relatedShift}px))
+  translateY(${(props) => (props.isUp ? getUpTransform(props) : '18px')});
   display: grid;
   grid-template-columns: ${({ size }) => `repeat(${size}, auto)`};
   grid-gap: 0 40px;
   animation: ${(props) => getAnimation(props)} 0.3s;
-
+  transition: all 0.3s;
   &:after,
   &:before {
     bottom: ${({ isUp }) => (isUp ? undefined : '100%')};
