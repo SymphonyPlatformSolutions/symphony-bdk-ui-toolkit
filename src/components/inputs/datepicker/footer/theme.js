0ac1513603fd2d8c157b534d9a57b6287ddd4de9
Anthony D <74194391+MADEiN06@users.noreply.github.com>
2020-12-04 10:43:30 +0100

    feat: improve DatePicker to have a footer navigation

---
+++ src/components/inputs/datepicker/footer/theme.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const FooterWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  margin: 26px 0 20px 0;
+`;
+
+export const Button = styled.div`
+  font-size: 12px;
+  line-height: 16px;
+  color: #008eff;
+  cursor: pointer;
+  text-transform: uppercase;
+`;
--- src/components/inputs/datepicker/samples/simple.js
+++ src/components/inputs/datepicker/samples/simple.js
@@ -35,3 +35,50 @@ export const RangeController = (props) => {
     />
   );
 };
+
+export const RangeNavigationController = (props) => {
+  const [currDate, setCurrDate] = useState({
+    startDate: null,
+    endDate: null,
+    isStart: true,
+  });
+  return (
+    <Datepicker
+      value={currDate.startDate}
+      endValue={currDate.endDate}
+      isStart={currDate.isStart}
+      onChange={setCurrDate}
+      isRange
+      hasFooterNavigation
+      numberOfMonths={2}
+      footerNavigationButtons={[
+        {
+          label: 'TODAY',
+          daysToSubstract: 0,
+        },
+        {
+          label: 'LAST 7 DAYS',
+          daysToSubstract: 7,
+        },
+        {
+          label: 'LAST 30 DAYS',
+          daysToSubstract: 30,
+        },
+      ]}
+      {...props}
+    />
+  );
+};
+
+export const RangeNavigation1MonthController = (props) => {
+  const [simpleDate, setSimpleDate] = useState(null);
+  return (
+    <Datepicker
+      value={simpleDate}
+      onChange={setSimpleDate}
+      hasFooterNavigation
+      footerNavigationButtons={[{ label: 'TODAY' }]}
+      {...props}
+    />
+  );
+};
