9490c277d8fc61642ee1a58c64c86cfab05c4ccf
Anthony D <74194391+MADEiN06@users.noreply.github.com>
2020-12-08 10:16:33 +0100

    fix: wrong style

--- src/components/inputs/datepickerv3/calendar/theme.js
+++ src/components/inputs/datepickerv3/calendar/theme.js
@@ -1,12 +1,5 @@
 import styled from 'styled-components';
 
-const getGridTemplateColumns = ({ activeMonths }) => {
-  return `repeat(${activeMonths.length}, 200px)`;
-};
-
 export const Wrapper = styled.div`
   width: 200px;
-  display: grid;
-  gridtemplatecolumns: ${getGridTemplateColumns};
-  gridgap: 0 64px;
 `;
