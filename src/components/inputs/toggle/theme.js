c90cd266c007b6e67d60e17ed6cafef948354116
Anthony D <74194391+MADEiN06@users.noreply.github.com>
2021-03-05 10:49:33 +0100

    feat: improving toggle component

--- src/components/inputs/toggle/theme.js
+++ src/components/inputs/toggle/theme.js
@@ -7,55 +7,39 @@ export const InvisibleInput = styled.input`
   position: absolute;
   width: 0;
 `;
+
 export const Wrapper = styled.div`
   cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
   margin: 5px 0;
-  width: 44px;
+  width: 32px;
 `;
+
 export const ToggleContainer = styled.div`
   display: flex;
   align-items: center;
-  width: 32px;
-  height: 11px;
+  width: 20px;
+  height: 8px;
   transition: all 0.3s;
   justify-content: center;
-  background-color: ${({ theme, disabled }) => (disabled ? theme.colors.grey_200 : theme.colors.grey_300)};
+  background-color: ${({ toggled }) => (toggled ? 'rgba(0,142,255,0.4)' : '#525760')};
   border-radius: 45px;
+  opacity: ${({ disabled }) => (disabled ? 0.4 : 1)}
   transform: translateX(10px);
-  ${Wrapper}:hover & {
-    background-color: ${({ theme, disabled }) => (disabled ? theme.colors.grey_200 : theme.colors.grey_400)};
-  }
   ${Wrapper}:focus-within   & {
     background-color: ${({ theme }) => theme.colors.grey_400};
   }
 `;
 
-const getColor = ({
-  theme, toggled, color, disabled,
-}, isBorder) => {
-  if (disabled) {
-    if (isBorder || toggled) {
-      return theme.colors.grey_400;
-    }
-    return theme.colors.grey_300;
-  }
-  if (!isBorder && !toggled) {
-    return theme.colors.grey_100;
-  }
-
-  if (color) {
-    return color;
-  }
-  return theme.colors.primary_500;
+const getCircleColor = ({ theme, toggled, color }) => {
+  return toggled ? color || theme.colors.primary_500 : theme.colors.white;
 };
 
 export const SwitchCircle = styled.div`
   border-radius: 50%;
-  width: 15px;
-  height: 15px;
-  background-color: ${props => getColor(props, false)};
-  border: ${props => `2px solid ${getColor(props, true)}`};
+  width: 16px;
+  height: 16px;
+  background-color: ${getCircleColor};
   transition: all 0.2s ease-out;
-  transform: translateX(${({ toggled }) => (toggled ? '16px' : '-16px')});
-  box-shadow: ${({ theme }) => (theme.mode === THEME_TYPES.DARK ? '' : '1px 2px 4px 0px rgba(0,0,0,0.15)')};
+  transform: translateX(${({ toggled }) => (toggled ? '8px' : '-8px')});
+  box-shadow: ${({ theme }) => (theme.mode === THEME_TYPES.DARK ? '' : '0px 1px 4px rgba(0, 0, 0, 0.32), 0px 0px 2px rgba(63, 63, 68, 0.64)')};
 `;
