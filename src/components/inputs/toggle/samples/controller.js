a0977d3a5a059604495dfcc336010e665e861b31
Anthony D <74194391+AnthonyDS-symphony@users.noreply.github.com>
2021-03-05 15:33:17 +0100

    Fix/sym 3767/settings modal (#166)
    * feat: reduce the CloseButton size on the modal
    
    * feat: improving toggle component
    
    Co-authored-by: Anthony D <74194391+MADEiN06@users.noreply.github.com>

--- src/components/inputs/toggle/samples/controller.js
+++ src/components/inputs/toggle/samples/controller.js
@@ -12,3 +12,15 @@ export const ToggleController = (props) => {
     />
   );
 };
+
+export const ToggleControllerBase = (props) => {
+  const [toggled, setToggle] = useState(false);
+
+  return (
+    <Toggle
+      {...props}
+      toggled={toggled}
+      onChange={setToggle}
+    />
+  );
+};
